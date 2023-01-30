cc097d3bd814b61d40ff39b9c7c128d115dacb1b
Mohammad hosseini <mohammad@samincoin.com>
2023-01-30 14:31:55 +0330

    nvim configs

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -22,3 +22,21 @@ vim.opt.spell = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+vim.opt.list = true
+vim.opt.listchars = { trail = '.' }
+vim.opt.fillchars:append({ eob = ' ' })
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.confirm = true
+
+vim.opt.undofile = true
+vim.opt.backup = true
+vim.opt.backupdir:remove('.')
+
